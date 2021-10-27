e21b4dd85f3f9a0d49bc6ae058625c3de6e4943a
Ayman Gado <aymangado@gmail.com>
2021-10-27 07:47:09 +0300

    Improvement

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -22,36 +22,53 @@ function get(url, callback) {
         callback(Http.response);
     }
 }
+
 function capitalizeFirstLetter(string) {
     return string.charAt(0).toUpperCase() + string.slice(1);
 }
 
+function closePhonebookForm() {
+    document.getElementById('phonebook_form').style.display = 'none';
+}
+
+function openPhonebookForm() {
+    document.getElementById('phonebook_form').style.display = 'block';
+}
+
 onLoad(function () {
     get('http://localhost:8000/api.php/phonebook', function (response) {
-        for(var i = 0; i < response.length; i++) {
-            var item = response[i];
+        if (response.length === 0) {
+            document.getElementById('no_phonebook_list').style.display = 'block';
+        } else {
+            for (var i = 0; i < response.length; i++) {
+                var item = response[i];
 
-            var tr = document.createElement('tr');
+                var tr = document.createElement('tr');
 
-            var id_td = document.createElement('td');
-            id_td.innerText = item.id;
-            tr.appendChild(id_td)
+                var id_td = document.createElement('td');
+                id_td.innerText = item.id;
+                tr.appendChild(id_td)
 
-            var full_name_td = document.createElement('td');
-            full_name_td.innerText = item.full_name;
-            tr.appendChild(full_name_td)
+                var full_name_td = document.createElement('td');
+                full_name_td.innerText = item.full_name;
+                tr.appendChild(full_name_td)
 
-            var numbers_td = document.createElement('td');
-            numbers_td.innerText = '';
-            for (var ii = 0; ii < item.phone_numbers.length; ii++) {
-                numbers_td.innerText += capitalizeFirstLetter(item.phone_numbers[ii]['type']) + ': ' + item.phone_numbers[ii]['phone_number'];
-                if (ii < item.phone_numbers.length - 1) {
-                    numbers_td.innerText += ' || ';
+                var numbers_td = document.createElement('td');
+                numbers_td.innerText = '';
+                for (var ii = 0; ii < item.phone_numbers.length; ii++) {
+                    numbers_td.innerText += capitalizeFirstLetter(item.phone_numbers[ii]['type']) + ': ' + item.phone_numbers[ii]['phone_number'];
+                    if (ii < item.phone_numbers.length - 1) {
+                        numbers_td.innerText += ' || ';
+                    }
+                }
+                tr.appendChild(numbers_td)
+
+                document.getElementById('phonebook_list').appendChild(tr);
+
+                if (i === response.length - 1) {
+                    document.getElementById('phonebook_list_table').style.display = 'block';
                 }
             }
-            tr.appendChild(numbers_td)
-
-            document.getElementById('phonebook_list').appendChild(tr);
         }
     });
 });
